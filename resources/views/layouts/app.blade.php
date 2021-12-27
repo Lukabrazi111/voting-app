7e974b2e5f9893ea4c3d219dd81286c6968966f1
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2021-12-28 00:20:58 +0400

    feat: notification styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,15 +25,138 @@
         @if (Route::has('login'))
             <div class="px-6 py-4">
                 @auth
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
+                    <div class="flex items-center space-x-4">
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
 
-                        <a class="hover:underline" href="{{ route('logout') }}"
-                           onclick="event.preventDefault();
+                            <a class="hover:underline" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
                                         this.closest('form').submit();">
-                            {{ __('Log Out') }}
-                        </a>
-                    </form>
+                                {{ __('Log Out') }}
+                            </a>
+                        </form>
+
+                        <div x-data="{ isOpen: false }" class="relative">
+                            <button @click="isOpen = !isOpen">
+                                <svg class="h-8 w-8 text-gray-400" fill="none" viewBox="0 0 24 24"
+                                     stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                          d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
+                                </svg>
+                                <span
+                                    class="absolute rounded-full bg-red text-white text-xss w-6 h-6 flex justify-center items-center -top-1 -right-1 border-2">8</span>
+                            </button>
+                            <ul
+                                x-cloak
+                                x-show="isOpen" x-transition.origin.top
+                                @click.outside="isOpen = false"
+                                @keydown.esc.window="isOpen = false"
+                                class="z-10 absolute w-76 md:w-96 text-left text-gray-700 bg-white text-sm shadow-dialog max-h-128 overflow-y-auto rounded-xl -right-28 md:-right-12"
+                            >
+
+                                <li>
+                                    <a
+                                        @click.prevent="
+                                        isOpen = false
+                                        "
+                                        href="#"
+                                        class="flex hover:bg-gray-100 px-5 py-3 transition duration-150 ease-in">
+
+                                        <img src="https://www.gravatar.com/avatar/c6ad550c8f30082474d1e58d20f67b3a"
+                                             class="rounded-xl w-10 h-10" alt="avatar">
+
+                                        <div class="ml-4">
+                                            <div class="line-clamp-6">
+                                                <span class="font-semibold">drehimself</span>
+                                                commented on
+                                                <span class="font-semibold">This is my idea</span>
+                                                <span>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci excepturi explicabo labore nemo sit. Ad dolores ea in iste quaerat veniam? Aliquid blanditiis corporis eius enim explicabo magnam mollitia nihil quam vero voluptatem. A consectetur corporis, eius fugiat repellendus saepe. Accusamus atque cumque dicta earum inventore maxime molestiae nobis porro quaerat quas rerum, sit. Commodi consectetur cumque distinctio ducimus ea excepturi impedit in modi mollitia nulla omnis possimus quia, reprehenderit sequi sit vel, vitae. Ad at corporis eius, id libero maiores molestiae molestias pariatur reiciendis sit veniam vero. Accusantium architecto dolores enim iusto neque quos saepe tempore veniam veritatis. Fugiat."</span>
+                                            </div>
+                                            <div class="text-xs text-gray-500 mt-2">1 hour ago</div>
+                                        </div>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a
+                                        @click.prevent="
+                                        isOpen = false
+                                        "
+                                        href="#"
+                                        class="flex hover:bg-gray-100 px-5 py-3 transition duration-150 ease-in">
+
+                                        <img src="https://www.gravatar.com/avatar/c6ad550c8f30082474d1e58d20f67b3a"
+                                             class="rounded-xl w-10 h-10" alt="avatar">
+
+                                        <div class="ml-4">
+                                            <div>
+                                                <span class="font-semibold">drehimself</span>
+                                                commented on
+                                                <span class="font-semibold">This is my idea</span>
+                                                <span>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cum delectus dicta incidunt laudantium totam veniam? Autem culpa dicta dolor enim incidunt inventore iste mollitia nihil numquam quidem! Earum, laborum?"</span>
+                                            </div>
+                                            <div class="text-xs text-gray-500 mt-2">1 hour ago</div>
+                                        </div>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a
+                                        @click.prevent="
+                                        isOpen = false
+                                        "
+                                        href="#"
+                                        class="flex hover:bg-gray-100 px-5 py-3 transition duration-150 ease-in">
+
+                                        <img src="https://www.gravatar.com/avatar/c6ad550c8f30082474d1e58d20f67b3a"
+                                             class="rounded-xl w-10 h-10" alt="avatar">
+
+                                        <div class="ml-4">
+                                            <div>
+                                                <span class="font-semibold">drehimself</span>
+                                                commented on
+                                                <span class="font-semibold">This is my idea</span>
+                                                <span>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cum delectus dicta incidunt laudantium totam veniam? Autem culpa dicta dolor enim incidunt inventore iste mollitia nihil numquam quidem! Earum, laborum?"</span>
+                                            </div>
+                                            <div class="text-xs text-gray-500 mt-2">1 hour ago</div>
+                                        </div>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a
+                                        @click.prevent="
+                                        isOpen = false
+                                        "
+                                        href="#"
+                                        class="flex hover:bg-gray-100 px-5 py-3 transition duration-150 ease-in">
+
+                                        <img src="https://www.gravatar.com/avatar/c6ad550c8f30082474d1e58d20f67b3a"
+                                             class="rounded-xl w-10 h-10" alt="avatar">
+
+                                        <div class="ml-4">
+                                            <div>
+                                                <span class="font-semibold">drehimself</span>
+                                                commented on
+                                                <span class="font-semibold">This is my idea</span>
+                                                <span>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cum delectus dicta incidunt laudantium totam veniam? Autem culpa dicta dolor enim incidunt inventore iste mollitia nihil numquam quidem! Earum, laborum?"</span>
+                                            </div>
+                                            <div class="text-xs text-gray-500 mt-2">1 hour ago</div>
+                                        </div>
+                                    </a>
+                                </li>
+
+                                <li class="border-t border-gray-300 text-center">
+                                    <button
+                                        href="#"
+                                        class="w-full block font-semibold hover:bg-gray-100 px-5 py-3 transition duration-150 ease-in">
+                                        Mark all as read
+                                    </button>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
                 @else
                     <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
